1d5340bc37cf6b933e6d124da6975ac7ccfbe8c7
Atila Santos <atila.romao@hotmail.com>
2022-04-17 20:49:53 -0300

    Implementing interfaces and examples of polymorphism

--- google-go-exercise/jedi5/exerc-4.go
+++ google-go-exercise/jedi5/exerc-4.go
@@ -2,10 +2,34 @@ package main
 
 import "fmt"
 
+type Human interface {
+	Eat()
+	Speak()
+	Walk()
+}
+
 type Person struct {
 	name  string
 	age   int
 	email string
+}
+
+type Chinese struct {
+	person Person
+	coin   string
+}
+
+type Brazilian struct {
+	person Person
+	coin   string
+}
+
+func (c Chinese) Eat() string {
+	return fmt.Sprintf("barata")
+}
+
+func (b Brazilian) Eat() string {
+	return fmt.Sprintf("brigadeiro")
 }
 
 func (p Person) Talk() string {
@@ -15,4 +39,10 @@ func (p Person) Talk() string {
 func main() {
 	person := Person{name: "Átila", age: 31, email: "atila.romao@hotmail.com"}
 	fmt.Println(person.Talk())
+
+	person2 := Brazilian{person: person, coin: "Real"}
+	person3 := Chinese{person: person, coin: "Ien"}
+
+	fmt.Println(person2.Eat())
+	fmt.Println(person3.Eat())
 }
